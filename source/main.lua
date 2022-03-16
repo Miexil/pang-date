871d0b76ed13ecd0423aabd6411cbeb7bda565b0
MBrunelle <maximilien.brunelle@gmail.com>
2022-03-16 12:40:57 +0100

    Added: Player movement handled in classes

--- source/main.lua
+++ source/main.lua
@@ -11,11 +11,14 @@ local centerX<const> = dsp.getWidth() / 2
 local centerY<const> = dsp.getHeight() / 2
 local playerSpriteHeight<const> = 50
 
-local fieldBoundary<const> = 30
-local maxGameSpeed<const> = 6
+fieldBoundary = 30
+maxGameSpeed = 6
+
+
+-- hard maxGameSpeed = 16 / aiSpeed = 30 / aiPrecision = 10 / ai.setSpeed+2
 
 local p1 = nil
-local ai = nil
+local p2 = nil
 
 local ballSprite = nil
 
@@ -24,8 +27,10 @@ local vy = math.random(3, maxGameSpeed)
 
 local function initalize()
   math.randomseed(playdate.getSecondsSinceEpoch())
-  p1 = Human(2, "images/plong-player", 10, centerY)
-  ai = Computer(5, "images/plong-player", 390, centerY)
+  -- p1 = Computer(1, 15, "images/plong-player", 10, centerY, 25, 1) -- EASY
+  -- p1 = Human(2, "images/plong-player", 10, centerY)
+  p1 = Computer(1, 20, "images/plong-player", 10, centerY, 1, 1)
+  p2 = Computer(1, 20, "images/plong-player", 390, centerY, 1, 1)
 
   ballSprite = gfx.sprite.new(gfx.image.new("images/plong-ball"))
   ballSprite:setCollideRect(0, 0, ballSprite:getSize())
@@ -41,109 +46,73 @@ function resetBall()
   ballSprite:moveTo(centerX, centerY)
 end
 
-initalize()
-
-function playerOneInputHandler()
-  local playerPos = p1.sprite.y;
-  if playdate.buttonIsPressed(playdate.kButtonUp) then
-    if playerPos > fieldBoundary then
-      p1:moveBy(0, -p1.speed)
-      if p1.speed < 15 then
-        p1.speed = p1.speed + 1
-      end
-    end
-  end
-  if playdate.buttonIsPressed(playdate.kButtonDown) then
-    if playerPos < (dsp.getHeight() - fieldBoundary) then
-      p1:moveBy(0, p1.speed)
-      if p1.speed < 15 then
-        p1.speed = p1.speed + 1
-      end
-    end
-  end
-  if playdate.buttonJustReleased(playdate.kButtonUp) or
-      playdate.buttonJustReleased(playdate.kButtonDown) then
-    p1.speed = 2
-  end
-
-  if playdate.buttonIsPressed(playdate.kButtonB) then
-    resetBall()
-  end
-end
-
-function moveAi()
-  if vy > 0 then
-    if ai.prevDir == 1 and ai.speed < 10 then ai.speed += 1
-    else ai.speed = 1 end
-    ai.prevDir = 1
-    if ai.sprite.y < (ballSprite.y) and ai.sprite.y <
-        (dsp.getHeight() - fieldBoundary) then
-      ai:moveBy(0, ai.speed)
-    else ai.speed = 1
-    end
-  else
-    if ai.prevDir == -1 and ai.speed < 10 then ai.speed += 1
-    else ai.speed = 1 end
-    ai.prevDir = -1
-    if ai.sprite.y > (ballSprite.y) and ai.sprite.y >
-        fieldBoundary then
-      ai:moveBy(0, -ai.speed)
-    else ai.speed = 1
-    end
-  end
-  print('ai speed : ' .. ai.speed)
-end
-
 function moveBall()
   ballSprite:moveTo(ballSprite.x + vx, ballSprite.y + vy)
 end
 
 function checkCollisions()
-  if ballSprite.y <= 2.5 or ballSprite.y >= 237.5 then
-    vy = -vy
+  print('ball x : ' .. ballSprite.x .. ' y : ' .. ballSprite.y .. ' vx : ' .. vx .. ' vy : ' .. vy)
+  if ballSprite.y <= 5 then
+    vy = math.abs(vy)
+  elseif ballSprite.y >= 235 then
+    vy = -math.abs(vy)
   end
   local collisions = ballSprite:overlappingSprites()
   if #collisions >= 1 then
     local collidingPlayer = collisions[1];
-    local ballX = ballSprite.x
-    local ballY = ballSprite.y
-    local playerY = collidingPlayer.y
-    local dif = playerY - ballY
+    local dif = collidingPlayer.y - ballSprite.y
     vx = -vx
-    if (vx < 0 and ballX > 0 and ballX < (centerX / 2)) then return end
-    if (vx > 0 and ballX < 0 and ballX > (centerX / 2)) then return end
+    if (vx < 0 and ballSprite.x > 0 and ballSprite.x < (centerX / 2)) then print('early return') return end
+    if (vx > 0 and ballSprite.x < 0 and ballSprite.x > (centerX / 2)) then print('early return') return end
     if dif > -4 and dif < 4 then
-      if vy > 0 then vy = 1 else vy = -1 end
+      print('middle')
+      if vy > 0 then vy = math.random(0,1) else vy = -math.random(0,1) end
     elseif dif > -18 and dif < 18 then
-      vy = vy - 1
+      print('middle-edge')
+      if dif < 0 then vy = math.abs(vy + math.random(1, 3))
+      else vy = -math.abs(vy - math.random(1, 3)) end
     else
+      print('edge')
       if dif < 0 then vy = math.abs(vy + math.random(4, 6))
       else vy = -math.abs(vy - math.random(4, 6)) end
     end
   end
 end
 
+function resetRound()
+  resetBall()
+  gfx.sprite.update()
+  drawScore();
+  gfx.drawText('*Round X*', centerX - 30, centerY + 50)
+  playdate.wait(1500)
+end
+
 function checkScore()
   if ballSprite.x <= 5 then
-    ai.score = ai.score + 1
-    resetBall()
+    p2.score = p2.score + 1
+    resetRound()
   end
   if ballSprite.x >= 395 then
     p1.score = p1.score + 1
-    resetBall()
+    resetRound()
   end
 end
 
+function drawScore()
+  gfx.drawText(p1.score, centerX - (centerX / 2), 5)
+  gfx.drawText(p2.score, centerX + (centerX / 2), 5)
+end
+
 function playdate.update()
-  playerOneInputHandler()
   checkCollisions()
-  moveAi()
+  p1:handleMovement(vx, vy, ballSprite)
+  p2:handleMovement(vx, vy, ballSprite)
   moveBall()
   checkScore()
 
   playdate.timer.updateTimers()
   gfx.sprite.update()
-
-  gfx.drawText(p1.score, centerX - (centerX / 2), 5)
-  gfx.drawText(ai.score, centerX + (centerX / 2), 5)
+  drawScore()
 end
+
+initalize()
--- source/player.lua
+++ source/player.lua
@@ -1,6 +1,7 @@
 import "CoreLibs/object"
 
 local gfx<const> = playdate.graphics
+local dsp<const> = playdate.display
 
 class('Player').extends()
 
@@ -18,23 +19,73 @@ function Player:setStartPosition(x, y)
   self.sprite:moveTo(x, y)
   self.sprite:add()
 end
-function Player:moveBy(x, y)
-  self.sprite:moveBy(x, y)
-end
+function Player:moveBy(x, y) self.sprite:moveBy(x, y) end
 
 class('Human').extends(Player)
 
 function Human:init(speed, spritePath, startPosX, startPosY)
-  Player.init(self, speed)
-  Player.loadSprite(self, spritePath)
-  Player.setStartPosition(self, startPosX, startPosY)
+  Human.super.init(self, speed)
+  Human.loadSprite(self, spritePath)
+  Human.setStartPosition(self, startPosX, startPosY)
+end
+function Human:handleMovement()
+  if playdate.buttonIsPressed(playdate.kButtonUp) then
+    printTable(self.sprite.y)
+    printTable(fieldBoundary)
+    if self.sprite.y > fieldBoundary then
+      self:moveBy(0, -self.speed)
+      if self.speed < 15 then self.speed = self.speed + 1 end
+    end
+  end
+  if playdate.buttonIsPressed(playdate.kButtonDown) then
+    if self.sprite.y < (dsp.getHeight() - fieldBoundary) then
+      self:moveBy(0, self.speed)
+      if self.speed < 15 then self.speed = self.speed + 1 end
+    end
+  end
+  if playdate.buttonJustReleased(playdate.kButtonUp) or
+      playdate.buttonJustReleased(playdate.kButtonDown) then self.speed = 2 end
+
+  if playdate.buttonIsPressed(playdate.kButtonB) then resetBall() end
 end
 
 class('Computer').extends(Player)
 
-function Computer:init(speed, spritePath, startPosX, startPosY)
+function Computer:init(minSpeed, maxSpeed, spritePath, startPosX, startPosY, precision, speedVelocity)
   self.prevDir = 0
-  Player.init(self, speed)
-  Player.loadSprite(self, spritePath)
-  Player.setStartPosition(self, startPosX, startPosY)
+  self.maxSpeed = maxSpeed
+  self.minSpeed = minSpeed
+  self.precision = precision or 20
+  self.speedVelocity = speedVelocity or 1
+  if startPosX < dsp.getWidth() / 2 then self.placement = 'left' else self.placement = 'right' end
+  Computer.super.init(self, minSpeed)
+  Computer.loadSprite(self, spritePath)
+  Computer.setStartPosition(self, startPosX, startPosY)
+end
+
+function Computer:setSpeed(prevDir, currentDir)
+  if prevDir == currentDir then
+    if self.speed < self.maxSpeed then
+      self.speed = self.speed + self.speedVelocity
+    end
+  else
+    self.speed = self.minSpeed
+  end
+end
+
+function Computer:handleMovement(vx, vy, ballSprite)
+  if (self.placement == 'left' and vx > 0) then self.speed = self.minSpeed return end
+  if (self.placement == 'right' and vx < 0) then self.speed = self.minSpeed  return end
+  if (ballSprite.y > (self.sprite.y - (self.precision + 15)) and ballSprite.y < (self.sprite.y + (self.precision + 15))) then print('do nothing') self.speed = self.minSpeed return end
+  if math.random(ballSprite.y - self.precision, ballSprite.y + self.precision) > self.sprite.y + math.random(0, self.precision) and self.sprite.y < (dsp.getHeight() - fieldBoundary) then
+    self:setSpeed(self.prevDir, 1)
+    self:moveBy(0, self.speed)
+    self.prevDir = 1
+  elseif math.random(ballSprite.y - self.precision, ballSprite.y + self.precision) < self.sprite.y - math.random(0, self.precision) and self.sprite.y > fieldBoundary then
+    self:setSpeed(self.prevDir, -1)
+    self:moveBy(0, -self.speed)
+    self.prevDir = -1
+  else
+    self.speed = self.minSpeed
+  end
 end
